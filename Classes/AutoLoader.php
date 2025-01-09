e0d126ecef8834a394b37ded37ec14abf9c224a3
RocmaDL <misterpoint0305@gmail.com>
2025-01-09 10:28:28 +0100

    Conception de l'AUTOLOADER

---
+++ Classes/AutoLoader.php
@@ -0,0 +1,13 @@
+<?php
+
+class AutoLoader{
+
+    static function register(){
+        spl_autoload_register(array(__CLASS__, 'autoload'));
+    }
+
+    static function autoload($class){
+        $class = str_replace('\\', '/', $class);
+        require 'Classes/' . $class . '.php';
+    }
+}
